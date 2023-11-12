db4cf89690c6dfc7efcc490503fad2b2b4bf11c4
Muneeb Arshad <42831583+mnbrshd@users.noreply.github.com>
2023-11-12 12:40:53 +0500

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,31 @@
+import yfinance as yf
+import streamlit as st
+from datetime import datetime
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock closing price and volume of Google!
+
+""")
+
+# define the ticker symbol
+tickerSymbol = 'GOOGL'
+
+# get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+# get the historical price for this ticker
+tickerDf = tickerData.history(
+    period='1d', start='2010-5-31', end=datetime.utcnow())
+# Open High     Low Close   Volume  Dividends   Stock Splits
+
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
